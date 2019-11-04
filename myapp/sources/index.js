af56ce142d56957a89e44020f3428d1b626cf57b
mauc <thomas.mauconduit@epitech.eu>
2019-11-04 11:57:29 +0000

    resolve some error with dockercomponent

--- myapp/sources/index.js
+++ myapp/sources/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello You!')
 })
 
 app.listen(8081, function () {
