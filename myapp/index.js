831ad9e2b326565ab5a8b7fa57d3e68fa055decf
mgaloux <mgaloux@localhost.localdomain>
2019-11-06 16:24:23 +0100

    all pages available

--- myapp/index.js
+++ myapp/index.js
@@ -8,6 +8,7 @@ app.use('/assets/vendor', express.static('./assets/vendor'));
 app.use('/assets/js', express.static('./assets/js'));
 
 app.use('/public', express.static('./public'))
+
 app.get('/', function (req, res) {
   res.sendfile('./public/index.html')
 })
