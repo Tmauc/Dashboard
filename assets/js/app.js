dc9af80f60aefe8f834286ea197375d664f959bf
mgaloux <mgaloux@localhost.localdomain>
2019-12-09 12:17:29 +0100

    widget

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+
+function addSteamModal() {
+
+}
+
+function addSteamWidget() {
+
+    const content = 
+    `<div class="steamwidget card mb-4 border-left-primary">
+        <div class="card-body py-3">
+            <div class="dropdown no-arrow mb-4 ">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-ellipsis-h"> Edit</i>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" onclick="removeWidget()">Delete</a>
+                </div>
+            </div>
+            <iframe src="https://store.steampowered.com/widget/383980/" frameborder="0" width ="646" height="190"></iframe>
+        </div>
+    </div>`;
+    $("#root").append(content);
+}
+
+async function callSteam(gamename) {
+    let appId = '';
+    axios.get('http://api.steampowered.com/ISteamApps/GetAppList/v0002/')
+    .then(async function (response) {
+        console.log(user);
+        appId = retrieveOui(response.data.applist, gamename);
+        appId = appId.appid.toString();
+        axios.get('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?format=json&appid=383980'/* + appId*/)
+    /*.then(function (response) {
+        callRiot(user, response.data.response.player_count)
+    })*/
+    });
+}
+
+function removeWidget() {
+    $(".steamwidget").remove();
+}
+
+function generateModal() {
+
+}
